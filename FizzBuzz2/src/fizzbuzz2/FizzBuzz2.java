b71bb7e3b27e6cb4bb19557bae6812feb82297bf
GabrielGuelsicct <2023345@student.cct.ie>
2025-03-24 13:33:18 +0000

    FizzBuzz done

--- FizzBuzz2/src/fizzbuzz2/FizzBuzz2.java
+++ FizzBuzz2/src/fizzbuzz2/FizzBuzz2.java
@@ -20,8 +20,23 @@ public class FizzBuzz2 {
         If a number is divisible by 3, print Fizz
         If a number is divisible by 5, print Buzz
         If a number is divisible by both, print FizzBuzz
-        
+
         */
+
+        for(int i = 1; i <= 150; i++){
+            if(i % 3 == 0 && i % 5 == 0){
+                System.out.println("FizzBuzz");
+            }
+            else if(i % 3 == 0){
+                System.out.println("Fizz");
+            }
+            else if(i % 5 == 0){
+                System.out.println("Buzz");
+            }
+            else{
+                System.out.println(i);
+            }
+        }
     }
     
 }
